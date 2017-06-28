648f26c5b32653973f529c204f5e82f151078ff0
crs2here <crs2here@gmail.com>
2017-06-27 18:33:32 -0700

    init commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to banquet.ninja! Buttery smooth banquet management for you and your customers.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
