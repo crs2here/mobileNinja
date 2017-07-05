6872e1d8ffd1d052a104e02f9773922519ab8309
crs2here <crs2here@gmail.com>
2017-07-04 21:42:35 -0700

    write auth  function to post to backend

---
+++ app/stores/authStore.js
@@ -0,0 +1,28 @@
+import { observable, action } from 'mobx'
+import apiBase from '../config/development';
+
+export default class AuthStore {
+  @observable authUser = null
+
+  constructor() {}
+
+  @action  // need to figure out mobx stuff
+  signIn({email, password}) {
+    (async () => {
+      try {  
+        const login = new apiBase;
+        let response = await fetch(`${login.url}/account/login`, {  
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },body: JSON.stringify({username,password})
+        });
+        return await response.json();
+      } catch(error) {
+        console.error(error);
+      }
+    })();
+  }
+}
+
