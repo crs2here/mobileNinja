076a6fd2c1219b79285166d6a42054a3122c93c8
crs2here <crs2here@gmail.com>
2017-07-04 21:42:07 -0700

    add configs for environments

---
+++ app/config/development.js
@@ -0,0 +1,4 @@
+export default class apiBase {
+  constructor(){}
+  url = "http://localhost:3001/api/v1";
+};
---
+++ app/config/production.js
@@ -0,0 +1,4 @@
+export default class apiBase {
+  constructor(){}
+  url = "http://restaurant.ninja/api/v1";
+};
