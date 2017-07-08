c68d3692fbdd4614d1c80d0d952dc8e36c812013
crs2here <crs2here@gmail.com>
2017-07-08 10:03:33 -0700

    change component name to events

--- app/scenes/eventScene.js
+++ app/scenes/eventScene.js
@@ -6,14 +6,14 @@ import { Container,
          Content, 
          List, 
          ListItem} from 'native-base';
-export default class BanquetScene extends Component {
+
+export default class EventScene extends Component {
   render() {
     var items = ['Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can','Mama Sakho','Emre Can'];
     return (
       <Container syt>
         <Content>
           <View style={styles.container}>
-            <Text style={styles.sectionHeader}>Banquet Ninja tempo header for second page that will have data</Text>
             <List dataArray={items} renderRow={(item) =>           
                 <ListItem>
                   <Text>{item}</Text>
@@ -31,15 +31,5 @@ const styles = StyleSheet.create({
   container: {
    flex: 1,
    paddingTop: 64
-  },
-  sectionHeader: {
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 10,
-    fontSize: 18,
-    fontWeight: 'bold',
-    backgroundColor: '#486C8F',
-    color: 'white',
-    textAlign: 'center'
   },
 })
