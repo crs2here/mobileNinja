7dd820da404c0360ae82d75900eaaa69c8459ac4
crs2here <crs2here@gmail.com>
2017-07-08 09:53:16 -0700

    move router to this page to handle lag

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Scene, Router} from "react-native-router-flux";
+import React, { Component } from "react"
 
-import Router from "./app/routers";
+// Scenes
+import MainScene from "./app/scenes/mainScene";
+import BanquetScene from "./app/scenes/banquetScene";
+
+// Stores
+import AuthStore from "./app/stores/authStore"
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router/>
+        <Router store={AuthStore}>
+            <Scene key="root">
+                <Scene 
+                    key="main" 
+                    component={MainScene}
+                    animation="fade"
+                    title="Welcome"
+                    navigationBarStyle={{backgroundColor:'#C0D0E4'}}
+                    initial
+                    />
+                <Scene 
+                    key="banquet" 
+                    component={BanquetScene} 
+                    animation="fade"
+                    title="Banquet.ninja info"
+                    navigationBarStyle={{backgroundColor:'#C0D0E4'}}
+                    />                        
+            </Scene>
+        </Router>
     );
   }
 }
