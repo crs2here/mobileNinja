a9edd07d92a764f921349938713658b7f9fcafe8
crs2here <crs2here@gmail.com>
2017-07-16 11:18:56 -0700

    clean up style sheet

--- app/components/eventDetailAccordion.js
+++ app/components/eventDetailAccordion.js
@@ -53,17 +53,6 @@ export default class EventDetailAccordion extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 22,
-    fontWeight: '300',
-    marginBottom: 20,
-  },
   header: {
     backgroundColor: '#F5FCFF',
     padding: 10,
@@ -76,29 +65,6 @@ const styles = StyleSheet.create({
   content: {
     padding: 20,
     backgroundColor: '#fff',
-  },
-  active: {
-    backgroundColor: 'rgba(255,255,255,1)',
-  },
-  inactive: {
-    backgroundColor: 'rgba(245,252,255,1)',
-  },
-  selectors: {
-    marginBottom: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  selector: {
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-  },
-  activeSelector: {
-    fontWeight: 'bold',
-  },
-  selectTitle: {
-    fontSize: 14,
-    fontWeight: '500',
-    padding: 10,
   },
 });
 
