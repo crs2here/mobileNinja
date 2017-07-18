7e9be8ce7e6d840076354fe35973e0405d398dc3
crs2here <crs2here@gmail.com>
2017-07-17 20:43:40 -0700

    make code more DRY, and add geteventDetail func

--- app/stores/eventStore.js
+++ app/stores/eventStore.js
@@ -3,24 +3,34 @@ import apiBase from '../config/development';
 export default class EventStore {
   
   constructor() {
+    this.base = new apiBase;
+    this.headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'x-access-token': ''          
+    }
   }
   // status, [fields]
   getEventData(token) {
-    const base = new apiBase;
+    this.headers['x-access-token'] = token;
     return new Promise((resolve, reject) =>{
       // when I don't have a headache make string dynamic
-      fetch(`${base.url}/events/contracts?where[status]=booked&sel=eventName+eventDate+startTime`, {
-        method: 'GET', 
-        headers:{
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'x-access-token': token          
-        }
-      }).then((res) =>{
-        resolve(res.json());
-      }).catch((error)=>{
-        reject(error);
-      }).done();      
+      fetch(`${this.base.url}/events/contracts?where[status]=booked&sel=eventName+eventDate+startTime`, {
+        method: 'GET', headers: this.headers})
+        .then((res) =>resolve(res.json()))
+        .catch((error)=>reject(error))
+        .done();      
+    });
+  }
+  getEventDetails(token, id) {
+    this.headers['x-access-token'] = token;
+    return new Promise((resolve, reject) =>{
+      // when I don't have a headache make string dynamic
+      fetch(`${this.base.url}/events/contracts/${id}`, {
+        method: 'GET', headers: this.headers})
+        .then((res) =>resolve(res.json()))
+        .catch((error)=>reject(error))
+        .done();      
     });
   }
 }
