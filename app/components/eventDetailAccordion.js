c5398135522ef4d35e9a531afbdc9b8dc5c09ce6
crs2here <crs2here@gmail.com>
2017-07-17 20:11:40 -0700

    add proper display off mocked data

--- app/components/eventDetailAccordion.js
+++ app/components/eventDetailAccordion.js
@@ -1,58 +1,122 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TouchableHighlight} from 'react-native';
 import { Container, Content, Text } from 'native-base';
+import moment from 'moment';
 import Collapsible from 'react-native-collapsible';
-
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { List, ListItem} from 'native-base';
 export default class EventDetailAccordion extends Component {
-  state = {
-    foodCollapsed: true,
-    notesCollapsed: true,
-    sequenceCollapsed: true, 
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      foodCollapsed: true,
+      notesCollapsed: true,
+      sequenceCollapsed: true,
+      isLoading: true, 
+    };    
+  }  
 
+  componentDidMount() {
+    const event = response.data;
+    const {eventDate, eventName, menuItems, venue, notes, eventSteps} = event;
+    const eventDetails = {eventDate, eventName, menuItems, venue, notes, eventSteps};
+    this.setState({
+      isLoading: false,
+      eventDetails
+    });  
+    // return {
+    //   _id,
+    //   eventDate,
+    //   eventName,
+    //   location: name || "no location set",
+    //   guestCount: 50              
+    // }
+  }  
+  // TODO: added list component now list data
   render() {
+    const {isLoading, eventDetails} = this.state;
+              {/* <Text>{eventDetails.menuItems[0].name}</Text>              
+              <Text>{eventDetails.menuItems[0].description}</Text>              
+              <Text>{eventDetails.menuItems[0].quantity}</Text>              
+              <Text>{eventDetails.menuItems[0].price}</Text>               */}
     return (
-      <Container style={{paddingTop: 64}}>
-        
-        <TouchableHighlight onPress={() => { this.setState({ foodCollapsed: !this.state.foodCollapsed });}}>
-          <View style={styles.header}>
-            <Text>Food</Text>
-          </View>
-        </TouchableHighlight>
-        <Collapsible collapsed={this.state.foodCollapsed} align="center">
-          <View style={styles.content}>
-            <Text>Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs</Text>
-          </View>
-        </Collapsible>
+      <View style={styles.container}>
+        {
+        (isLoading)
+        ? <Text> Something went wrong </Text> 
+        : 
+        <Container>
+          <TouchableHighlight onPress={() => { this.setState({ foodCollapsed: !this.state.foodCollapsed });}}>
+            <View style={styles.header}>
+              <Text>Food</Text>
+            </View>
+          </TouchableHighlight>
+          <Collapsible collapsed={this.state.foodCollapsed} align="center">
+            <View style={styles.content}>
+              <List dataArray={eventDetails.menuItems} renderRow={(food) =>
+                <ListItem onPress={() => { console.log(event._id) }}>
+                  <Grid>
+                    <Col>
+                      <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+                        <Text style={{alignSelf:'flex-start'}}>item: {food.name}</Text>
+                        <Text style={{alignSelf:'flex-end'}}>price: {food.price}</Text>
+                        <Text style={{alignSelf:'flex-end'}}>quantity: {food.quantity}</Text>
+                      </View>
+                    </Col>
+                  </Grid>
+                </ListItem>
+              }></List>              
+            </View>
+          </Collapsible>
 
-        <TouchableHighlight  onPress={() => { this.setState({ notesCollapsed: !this.state.notesCollapsed });}}>
-          <View style={styles.header}>
-            <Text>Notes</Text>
-          </View>
-        </TouchableHighlight>
-        <Collapsible collapsed={this.state.notesCollapsed} align="center">
-          <View style={styles.content}>
-            <Text>Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs</Text>
-          </View>
-        </Collapsible>
+          <TouchableHighlight  onPress={() => { this.setState({ notesCollapsed: !this.state.notesCollapsed });}}>
+            <View style={styles.header}>
+              <Text>Notes</Text>
+            </View>
+          </TouchableHighlight>
+          <Collapsible collapsed={this.state.notesCollapsed} align="center">
+            <View style={styles.content}>
+              <Text style={{alignSelf:'flex-start'}}>{eventDetails.notes}</Text>
+            </View>
+          </Collapsible>
 
-        <TouchableHighlight  onPress={() => { this.setState({ sequenceCollapsed: !this.state.sequenceCollapsed });}}>
-          <View style={styles.header}>
-            <Text>Sequence</Text>
-          </View>
-        </TouchableHighlight>
-        <Collapsible collapsed={this.state.sequenceCollapsed} align="center">
-          <View style={styles.content}>
-            <Text>Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs</Text>
-          </View>
-        </Collapsible>
-
-      </Container>
+          <TouchableHighlight  onPress={() => { this.setState({ sequenceCollapsed: !this.state.sequenceCollapsed });}}>
+            <View style={styles.header}>
+              <Text>Sequence</Text>
+            </View>
+          </TouchableHighlight>
+          <Collapsible collapsed={this.state.sequenceCollapsed} align="center">
+            <View style={styles.content}>
+              <List dataArray={eventDetails.eventSteps} renderRow={(sequence) =>
+                <ListItem onPress={() => { console.log(event._id) }}>
+                  <Grid>
+                    <Col>
+                      <View style={{justifyContent:'space-between'}}>
+                        <Text style={{alignSelf:'flex-start'}}>time: {moment(sequence.time).format("h:mm a")}</Text>
+                        <Text style={{alignSelf:'flex-start'}}>duration: {sequence.duration}</Text>
+                        <Text style={{alignSelf:'flex-start'}}>{sequence.description}</Text>
+                      </View>
+                    </Col>
+                  </Grid>
+                </ListItem>
+              }></List>                 
+            </View>
+          </Collapsible>
+        </Container>
+      }
+      </View> 
     );
   }
 }
+              // <Text>{moment(eventDetails.eventSteps[0]).format("h:mm a")}</Text>
+              // <Text>{eventDetails.eventSteps[0].duration}</Text>
+              // <Text>{eventDetails.eventSteps[0].description}</Text>
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 64
+  },  
   header: {
     backgroundColor: '#F5FCFF',
     padding: 10,
@@ -68,7 +132,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const tempData = {
+const response = {
   "data": {
     "_id": "5957d62ffdd346e5f5cd53f0",
     "customer": {
@@ -178,5 +242,3 @@ const tempData = {
     }
   }
 };
-
-const BACON_IPSUM = 'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
