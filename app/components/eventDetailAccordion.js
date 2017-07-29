cf68b2ed644df5e7b1fb46b09ef8052ed319eadc
crs2here <crs2here@gmail.com>
2017-07-29 14:08:30 -0700

    add style for data format

--- app/components/eventDetailAccordion.js
+++ app/components/eventDetailAccordion.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TouchableHighlight, AsyncStorage} from 'react-native';
-import { Container, Content, Text } from 'native-base';
+import { StyleSheet, View, TouchableHighlight, AsyncStorage, Text} from 'react-native';
+import { Container, Content } from 'native-base';
 import Collapsible from 'react-native-collapsible';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import { List, ListItem} from 'native-base';
@@ -23,20 +23,22 @@ export default class EventDetailAccordion extends Component {
       <Container>
         <TouchableHighlight onPress={() => { this.setState({ foodCollapsed: !this.state.foodCollapsed });}}>
           <View style={styles.header}>
-            <Text>Food</Text>
+            <Text style={styles.accordionHdr}>Food</Text>
           </View>
         </TouchableHighlight>
         <Collapsible collapsed={this.state.foodCollapsed} align="center">
           <View style={styles.content}>
             {(Array.isArray(eventDetails.food)) ? 
                 <List dataArray={eventDetails.food} renderRow={(food) =>
-                  <ListItem onPress={() => { console.log(event._id) }}>
+                  <ListItem>
                     <Grid>
                       <Col>
                         <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
-                          <Text style={{alignSelf:'flex-start'}}>item: {food.name}</Text>
-                          <Text style={{alignSelf:'flex-end'}}>price: {food.price}</Text>
-                          <Text style={{alignSelf:'flex-end'}}>quantity: {food.quantity}</Text>
+                          <Text style={{alignSelf:'flex-start', fontWeight: 'bold'}}>{food.name}</Text>
+                          <Text style={{alignSelf:'flex-end', fontWeight: 'bold'}}>{food.quantity}@{food.price}</Text>
+                        </View>
+                        <View style={{flexDirection: 'row', justifyContent:'space-between'}}>  
+                          <Text style={{alignSelf:'flex-start', fontStyle:'italic', paddingTop: 10}}>{food.description}</Text>
                         </View>
                       </Col>
                     </Grid>
@@ -48,7 +50,7 @@ export default class EventDetailAccordion extends Component {
 
         <TouchableHighlight  onPress={() => { this.setState({ notesCollapsed: !this.state.notesCollapsed });}}>
           <View style={styles.header}>
-            <Text>Notes</Text>
+            <Text style={styles.accordionHdr}>Notes</Text>
           </View>
         </TouchableHighlight>
         <Collapsible collapsed={this.state.notesCollapsed} align="center">
@@ -59,23 +61,30 @@ export default class EventDetailAccordion extends Component {
 
         <TouchableHighlight  onPress={() => { this.setState({ sequenceCollapsed: !this.state.sequenceCollapsed });}}>
           <View style={styles.header}>
-            <Text>Sequence</Text>
+            <Text style={styles.accordionHdr}>Sequence</Text>
           </View>
         </TouchableHighlight>
         <Collapsible collapsed={this.state.sequenceCollapsed} align="center">
           <View style={styles.content}>
             {(Array.isArray(eventDetails.sequence)) ? 
               <List dataArray={eventDetails.sequence} renderRow={(sequence) =>
-                <ListItem onPress={() => { console.log(event._id) }}>
-                  <Grid>
-                    <Col>
-                      <View style={{justifyContent:'space-between'}}>
-                        <Text style={{alignSelf:'flex-start'}}>time: {moment(sequence.time).format("h:mm a")}</Text>
-                        <Text style={{alignSelf:'flex-start'}}>duration: {sequence.duration}</Text>
-                        <Text style={{alignSelf:'flex-start'}}>{sequence.description}</Text>
-                      </View>
+                <ListItem>
+                  <Grid style={{ backgroundColor: '#F5F5F5'}}>
+                    <Col style={{backgroundColor: '#486C8F', height: 70, width: 75}}>
+                      <Text style={styles.dateBoxText}> {moment(sequence.time).format("h:mm a")} </Text>  
+                    </Col>                    
+                    <Col style={{ backgroundColor: '#F5F5F5' }}>
+                      <Grid>
+                        <Col> 
+                          <Text style={{fontWeight: 'bold', paddingLeft: 5}}>Duration:</Text>
+                        </Col>
+                        <Col>                             
+                          <Text style={{alignSelf:'flex-end', fontStyle:'italic', paddingRight: 5}}>{sequence.duration}</Text>     
+                        </Col>
+                      </Grid>
+                      <Text style={styles.sequenceDetails}>{sequence.description}</Text>                       
                     </Col>
-                  </Grid>
+                  </Grid>                 
                 </ListItem>
               }></List>
             : <Text>{eventDetails.sequence}</Text>}                      
@@ -104,4 +113,18 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#fff',
   },
+  accordionHdr: {
+    fontWeight: 'bold', 
+    fontSize: 16
+  },
+  dateBoxText : {
+    paddingTop:30,
+    color: 'white',
+    alignSelf:'center',
+    fontWeight: 'bold',
+  },
+  sequenceDetails : {
+    fontStyle:'italic',
+    paddingLeft: 5
+  }  
 });
